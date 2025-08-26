89bdd199fe2cb303fa31582b7acd3b35e8069252
Moua Yang <mouahyang10@gmail.com>
2025-08-25 19:18:30 -0500

    go

--- Migrations/20250826001752_SyncModel.Designer.cs
+++ Migrations/20250826001752_SyncModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Quotely.Api.Data;
@@ -11,9 +12,11 @@ using Quotely.Api.Data;
 namespace Quotely.Api.Migrations
 {
     [DbContext(typeof(AppDb))]
-    partial class AppDbModelSnapshot : ModelSnapshot
+    [Migration("20250826001752_SyncModel")]
+    partial class SyncModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
